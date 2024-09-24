7329695833534c70280ae79196341bec78ad9673
paradox8599 <paradox8599@outlook.com>
2024-09-24 21:04:40 +1000

    wezterm keymaps for mux

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,5 +1,6 @@
 local wezterm = require("wezterm")
 local config = wezterm.config_builder()
+local action = wezterm.action
 
 ----------------------------------------------------------------
 ----------------------------------------------------------------
@@ -14,6 +15,16 @@ end
 
 ----------------------------------------------------------------
 ----------------------------------------------------------------
+
+config.use_fancy_tab_bar = false
+config.hide_tab_bar_if_only_one_tab = true
+config.initial_cols = 120
+config.initial_rows = 32
+
+config.macos_window_background_blur = 40
+config.window_background_opacity = 0.7
+
+config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
 
 config.color_scheme = "Arthur (Gogh)"
 -- config.color_scheme = 'Bespin (base16)'
@@ -33,23 +44,25 @@ if is_win() then
 	config.font_size = 10.0
 	config.default_prog = { "pwsh", "--nologo" }
 	config.window_decorations = "RESIZE"
+
+	config.leader = { key = "a", mods = "CTRL", timeout_milliseconds = 1000 }
+	config.keys = {
+		{ key = "b", mods = "LEADER", action = action.SplitHorizontal({ domain = "CurrentPaneDomain" }) },
+		{ key = "v", mods = "LEADER", action = action.SplitVertical({ domain = "CurrentPaneDomain" }) },
+		{ key = "h", mods = "LEADER|CTRL", action = action.ActivatePaneDirection("Left") },
+		{ key = "l", mods = "LEADER|CTRL", action = action.ActivatePaneDirection("Right") },
+		{ key = "j", mods = "LEADER|CTRL", action = action.ActivatePaneDirection("Down") },
+		{ key = "k", mods = "LEADER|CTRL", action = action.ActivatePaneDirection("Up") },
+		{ key = "x", mods = "LEADER|CTRL", action = action.CloseCurrentPane({ confirm = true }) },
+	}
 end
 
 config.mouse_bindings = {
 	{
 		event = { Up = { streak = 1, button = "Left" } },
 		mods = "NONE",
-		action = wezterm.action.CompleteSelection("ClipboardAndPrimarySelection"),
+		action = action.CompleteSelection("ClipboardAndPrimarySelection"),
 	},
 }
-
-config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
-
-config.macos_window_background_blur = 40
-config.window_background_opacity = 0.7
-config.initial_cols = 120
-config.initial_rows = 32
-
-config.hide_tab_bar_if_only_one_tab = true
 
 return config
