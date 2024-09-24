c7f367cf002637540693eafc5b3eb9e15c1b0971
paradox8599 <paradox8599@outlook.com>
2024-09-24 20:47:37 +1000

    wezterm simplify config init

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,8 +1,5 @@
 local wezterm = require("wezterm")
-local config = {}
-if wezterm.config_builder then
-	config = wezterm.config_builder()
-end
+local config = wezterm.config_builder()
 
 ----------------------------------------------------------------
 ----------------------------------------------------------------
