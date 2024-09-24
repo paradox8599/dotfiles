9b857d8a934b94afdaea6b7237d5ea0293234376
paradox8599 <paradox8599@outlook.com>
2024-09-24 18:52:52 +1000

    wezterm configs

--- .wezterm.lua
+++ .wezterm.lua
@@ -11,20 +11,29 @@ local is_darwin = function()
 	return wezterm.target_triple:find("darwin") ~= nil
 end
 
+local is_win = function()
+	return wezterm.target_triple:find("windows") ~= nil
+end
+
 ----------------------------------------------------------------
 ----------------------------------------------------------------
 
-config.color_scheme = "Adventure"
+config.color_scheme = "Bamboo"
+-- config.color_scheme = 'Bitmute (terminal.sexy)'
+-- config.color_scheme = 'Breeze (Gogh)'
 
 if is_darwin() then
 	config.font_size = 13.0
-else
-	config.font_size = 12.0
+end
+
+if is_win then
+	config.font_size = 10.0
+	config.default_prog = { "pwsh", "--nologo" }
 end
 
 config.font = wezterm.font("JetBrainsMono Nerd Font", { weight = "Medium" })
 
-config.window_background_opacity = 0.98
+config.window_background_opacity = 0.95
 config.initial_cols = 120
 config.initial_rows = 32
 
