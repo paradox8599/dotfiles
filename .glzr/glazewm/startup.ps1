0dbb6598dc5001cc6a8cb7e01fbadeba75ea3a5b
paradox8599 <paradox8599@outlook.com>
2025-04-01 03:24:46 +1100

    glazewm config

--- .glzr/glazewm/startup.ps1
+++ .glzr/glazewm/startup.ps1
@@ -2,4 +2,5 @@ get-process zebar -ErrorAction SilentlyContinue | stop-process -force
 get-process gat-gwm -ErrorAction SilentlyContinue | stop-process -force
 
 conhost.exe --headless zebar
+&"$HOME\.glzr\glazewm\gat-gwm.exe"
 
